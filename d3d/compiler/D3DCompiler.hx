73e762c6de25ddc785e3c2dcee68c7e95dac0e35
Aidan63 <aidan.lee63@gmail.com>
2018-08-14 19:38:56 +0100

    Fixed incorrect blob interface name

--- d3d/compiler/D3DCompiler.hx
+++ d3d/compiler/D3DCompiler.hx
@@ -1,38 +1,15 @@
 package d3d.compiler;
 
-import haxe.io.Bytes;
-
 @:include("D3Dcompiler.h")
-extern class D3DCompiler
+class D3DCompiler
 {
-    inline static function compileFromFile(_filename : String, _entryPoint : String, _target : String, _btyecodeOut : Blob, _errors : Blob) : Int
+    public static function compileFromFile(_filename : String, _entryPoint : String, _target : String, _btyecodeOut : Blob, _errors : Blob) : Int
     {
-        return untyped __cpp__('D3DCompileFromFile(
-            {0}.__WCStr(),
-            0,
-            0,
-            {1},
-            {2},
-            0,
-            0,
-            (ID3DBlob**)&{3},
-            (ID3DBlob**)&{4})', _filename, _entryPoint, _target, _btyecodeOut, _errors);
+        return untyped __cpp__('D3DCompileFromFile({0}.__WCStr(), NULL, NULL, {1}, {2}, 0, 0, (ID3DBlob**)&{3}, (ID3DBlob**)&{4})', _filename, _entryPoint, _target, _btyecodeOut, _errors);
     }
 
-    inline static function compile(_data : Bytes, _entryPoint : String, _target : String, _btyecodeOut : Blob, _errors : Blob)
+    public static function compile(_data : String, _entryPoint : String, _target : String, _btyecodeOut : Blob, _errors : Blob) : Int
     {
-        return untyped __cpp__('D3DCompile(
-            (const void *)&({0}[0]),
-            {1},
-            0,
-            0,
-            0,
-            {2},
-            {3},
-            0,
-            0,
-            (ID3D11Blob**)&{4},
-            (ID3D11Blob**)&{5}
-        )', _data.getData(), _data.length, _entryPoint, _target, _btyecodeOut, _errors);
+        return untyped __cpp__('D3DCompile({0}.c_str(), {1}, NULL, NULL, NULL, {2}, {3}, 0, 0, (ID3DBlob**)&{4}, (ID3DBlob**)&{5})', _data, _data.length, _entryPoint, _target, _btyecodeOut, _errors);
     }
 }
