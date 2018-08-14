52021aaafed2b265cdc3926a611bb1aa36043314
Aidan <aidan.lee63@gmail.com>
2018-08-14 15:05:50 +0100

    Added compile command for D3DCompiler

--- d3d/compiler/D3DCompiler.hx
+++ d3d/compiler/D3DCompiler.hx
@@ -1,5 +1,7 @@
 package d3d.compiler;
 
+import haxe.io.Bytes;
+
 @:include("D3Dcompiler.h")
 extern class D3DCompiler
 {
@@ -15,5 +17,22 @@ extern class D3DCompiler
             0,
             (ID3DBlob**)&{3},
             (ID3DBlob**)&{4})', _filename, _entryPoint, _target, _btyecodeOut, _errors);
+    }
+
+    inline static function compile(_data : Bytes, _entryPoint : String, _target : String, _btyecodeOut : Blob, _errors : Blob)
+    {
+        return untyped __cpp__('D3DCompile(
+            (const void *)&({0}[0]),
+            {1},
+            0,
+            0,
+            0,
+            {2},
+            {3},
+            0,
+            0,
+            (ID3D11Blob**)&{4},
+            (ID3D11Blob**)&{5}
+        )', _data.getData(), _data.length, _entryPoint, _target, _btyecodeOut, _errors);
     }
 }
